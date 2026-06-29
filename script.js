22342dc155e67e41a62ac8f8b35faa519af3272e
THARUN77650 <shashidharsk43@gmail.com>
2026-06-30 01:51:40 +0530

    Refactor lead form submission event handler

--- script.js
+++ script.js
@@ -2,20 +2,15 @@ document.getElementById("leadForm").addEventListener("submit", function(e){
 
 e.preventDefault();
 
-let name = document.getElementById("name").value;
-let phone = document.getElementById("phone").value;
-let service = document.getElementById("service").value;
-let message = document.getElementById("message").value;
+let name=document.getElementById("name").value;
+let phone=document.getElementById("phone").value;
+let service=document.getElementById("service").value;
+let message=document.getElementById("message").value;
 
-// WhatsApp Lead Send
-let url = `https://wa.me/917702976853?text=
-New Lead:%0A
-Name: ${name}%0A
-Phone: ${phone}%0A
-Service: ${service}%0A
-Message: ${message}`;
+let url=`https://wa.me/917702976853?text=
+New Lead:%0AName:${name}%0APhone:${phone}%0AService:${service}%0AMessage:${message}`;
 
-window.open(url, "_blank");
+window.open(url,"_blank");
 
 alert("Thank you! We will contact you soon.");
 
